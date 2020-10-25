f8a077b49402945ed0ac00ff81bdce78afc26594
soheeyu <ysh7271@naver.com>
2020-10-25 18:10:12 +0900

    2020-10-25

---
+++ Vuejs/vue01docs/aa.js
@@ -0,0 +1,48 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: { 
+
+    },
+    //template: ``, //실제로 template를 사용하지는 않는다
+    methods: { //등기식 함수
+
+    },
+    components: { }, 
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted: function() {
+        console.log("mounted");
+    },
+    updated: function() {
+        console.log("updated");
+    },
+});
+
+Vue.component("aa", { 
+    /* pdtmc^2w */
+    props: [] ,
+    data: function() { //컴포넌트에서는 데이터를 함수로 만들어야한다!
+        return { }
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
